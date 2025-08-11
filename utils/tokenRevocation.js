e7081fe5d9e492c3a42763d3b40a4f855241e60d
Innowide Tech <innowidetech@gmail.com>
2025-08-11 22:06:57 +0530

    Delete utils directory

--- utils/tokenRevocation.js
+++
@@ -1,9 +0,0 @@
-let revokedTokens = [];
-
-exports.addRevokedToken = (token) => {
-  revokedTokens.push(token);
-};
-
-exports.isTokenRevoked = (token) => {
-  return revokedTokens.includes(token);
-};
